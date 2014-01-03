a454f8034849caf7680153c306faf75db7b80194
Matthias Le Brun <mlbli@me.com>
2014-01-03 14:23:10 +0100

    ES3 compat

--- index.js
+++ index.js
@@ -1,77 +1,93 @@
-var _hasOwnProperty = {}.hasOwnProperty
-  , enumBugProperties = [
-        "constructor"
-      , "hasOwnProperty"
-      , "isPrototypeOf"
-      , "propertyIsEnumerable",
-      , "toLocaleString"
-      , "toString"
-      , "valueOf"
-    ]
-  , hasEnumBug = !Object.prototype.propertyIsEnumerable.call({constructor:1}, "constructor")
-  , each = function(object, fn, thisValue){
-      var key, index, length, enumKey
-      if(!object) return
-      for(key in object) {
-        if(_hasOwnProperty.call(object, key)) {
-          fn.call(thisValue, object[key], key, object)
-        }
-      }
-      if(hasEnumBug) {
-        index = -1
-        length = enumBugProperties.length
-        while(++index < length) {
-          key = enumBugProperties[index]
+;(function(root, name, output){
+  if(typeof define == "function" && define.amd) return define([], output)
+  if(typeof module == "object" && module.exports) module.exports = output()
+  else root[name] = output()
+})(this, "klass", function(){
+  
+  var classExports = {}
+    , _hasOwnProperty = {}.hasOwnProperty
+    , enumBugProperties = [
+          "constructor"
+        , "hasOwnProperty"
+        , "isPrototypeOf"
+        , "propertyIsEnumerable",
+        , "toLocaleString"
+        , "toString"
+        , "valueOf"
+      ]
+    , hasEnumBug = !Object.prototype.propertyIsEnumerable.call({constructor:1}, "constructor")
+    , each = function(object, fn, thisValue){
+        var key, index, length, enumKey
+        if(!object) return
+        for(key in object) {
           if(_hasOwnProperty.call(object, key)) {
             fn.call(thisValue, object[key], key, object)
           }
         }
+        if(hasEnumBug) {
+          index = -1
+          length = enumBugProperties.length
+          while(++index < length) {
+            key = enumBugProperties[index]
+            if(_hasOwnProperty.call(object, key)) {
+              fn.call(thisValue, object[key], key, object)
+            }
+          }
+        }
       }
+    , objectCreate = Object.create || function(object){
+        function F(){}
+        F.prototype = object
+        return new F()
+      }
+    , klassId = -1
+    , intancesIds = {}
+    , klassesMap = {}
+  
+  classExports._instances = {}
+  
+  classExports.create = function(){
+    var self = this
+      , instance = objectCreate(self)
+      , instanceId
+    if(_hasOwnProperty.call(self, "constructor") &&
+        typeof instance.constructor == "function") {
+  
+      instance.constructor.apply(instance, arguments)
     }
-  , klassId = -1
-  , intancesIds = {}
-  , klassesMap = {}
-
-exports._instances = {}
-
-exports.create = function(){
-  var self = this
-    , instance = Object.create(self)
-    , instanceId
-  if(_hasOwnProperty.call(self, "constructor") &&
-      typeof instance.constructor == "function") {
-
-    instance.constructor.apply(instance, arguments)
+    instanceId = ++intancesIds[self._klass]
+    instance._id = instanceId
+    instance._instances[instanceId] = instance
+    return instance
   }
-  instanceId = ++intancesIds[self._klass]
-  instance._id = instanceId
-  instance._instances[instanceId] = instance
-  return instance
-}
-
-exports.destroy = function(){
-  var instance = this
-    , parent = klassesMap[instance._klass]
-  if(_hasOwnProperty.call(instance, "_klass")) {
-    return
+  
+  classExports.destroy = function(){
+    var instance = this
+      , parent = klassesMap[instance._klass]
+    if(_hasOwnProperty.call(instance, "_klass")) {
+      return
+    }
+    delete instance._instances[instance._id]
+    if(_hasOwnProperty.call(parent || {}, "destructor") &&
+        typeof instance.destructor == "function") {
+      instance.destructor.apply(instance, arguments)
+    }
   }
-  delete instance._instances[instance._id]
-  if(_hasOwnProperty.call(parent || {}, "destructor") &&
-      typeof instance.destructor == "function") {
-    instance.destructor.apply(instance, arguments)
+  
+  classExports.extend = function(object){
+    var klass = objectCreate(this)
+    each(object, extendCallback, klass)
+    klass._instances = {}
+    klass._klass = ++klassId
+    intancesIds[klassId] = -1
+    klassesMap[klassId] = klass
+    return klass
   }
-}
+  
+  function extendCallback(item, key){
+    this[key] = item
+  }
 
-exports.extend = function(object){
-  var klass = Object.create(this)
-  each(object, extendCallback, klass)
-  klass._instances = {}
-  klass._klass = ++klassId
-  intancesIds[klassId] = -1
-  klassesMap[klassId] = klass
-  return klass
-}
+  return classExports
 
-function extendCallback(item, key){
-  this[key] = item
-}
+})
