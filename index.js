fc126ae3a09369ce4c0c0f2f2c9c2c2bb490757e
Matthias Le Brun <mlbli@me.com>
2014-03-14 14:47:16 +0100

    1.2.0

--- index.js
+++ index.js
@@ -1,88 +1,28 @@
-;(function(root, name, output){
-  if(typeof define == "function" && define.amd) return define([], output)
-  if(typeof module == "object" && module.exports) module.exports = output()
-  else root[name] = output()
-})(this, "klass", function(){
-  
-  var classExports = {}
-    , _hasOwnProperty = {}.hasOwnProperty
-    , enumBugProperties = [
-          "constructor"
-        , "hasOwnProperty"
-        , "isPrototypeOf"
-        , "propertyIsEnumerable",
-        , "toLocaleString"
-        , "toString"
-        , "valueOf"
-      ]
-    , hasEnumBug = !Object.prototype.propertyIsEnumerable.call({constructor:1}, "constructor")
-    , each = function(object, fn, thisValue){
-        var key, index, length, enumKey
-        if(!object) return
-        for(key in object) {
-          if(_hasOwnProperty.call(object, key)) {
-            fn.call(thisValue, object[key], key, object)
-          }
-        }
-        if(hasEnumBug) {
-          index = -1
-          length = enumBugProperties.length
-          while(++index < length) {
-            key = enumBugProperties[index]
-            if(_hasOwnProperty.call(object, key)) {
-              fn.call(thisValue, object[key], key, object)
-            }
-          }
-        }
-      }
-    , objectCreate = Object.create || function(object){
-        function F(){}
-        F.prototype = object
-        return new F()
-      }
-    , klassId = -1
-    , intancesIds = {}
-    , klassesMap = {}
-    
-  classExports.create = function(){
-    var self = this
-      , instance = objectCreate(self)
-      , instanceId
-    if(_hasOwnProperty.call(self, "constructor") &&
-        typeof instance.constructor == "function") {
-  
+var extend = require("bloody-collections/lib/extend")
+  , hasMethod = require("./lib/hasMethod")
+  , create = require("./lib/create")
+  , K = function(){}
+
+module.exports = {
+  extend : function(object){
+    var subKlass = create(this)
+    extend(subKlass, object)
+    if(!hasMethod(subKlass, "constructor")) {
+      subKlass.constructor =
+        typeof subKlass.constructor == "function" ? subKlass.constructor : K
+    }
+    return subKlass
+  },
+  create : function(){
+    var instance = create(this)
+    if(hasMethod(instance, "constructor")) {
       instance.constructor.apply(instance, arguments)
     }
-    instanceId = ++intancesIds[self._klass]
-    instance._id = instanceId
     return instance
-  }
-  
-  classExports.destroy = function(){
-    var instance = this
-      , parent = klassesMap[instance._klass]
-    if(_hasOwnProperty.call(instance, "_klass")) {
-      return
-    }
-    if(_hasOwnProperty.call(parent || {}, "destructor") &&
-        typeof instance.destructor == "function") {
-      instance.destructor.apply(instance, arguments)
+  },
+  destroy : function(){
+    if(typeof this.destructor == "function") {
+      this.destructor.apply(this, arguments)
     }
   }
-  
-  classExports.extend = function(object){
-    var klass = objectCreate(this)
-    each(object, extendCallback, klass)
-    klass._klass = ++klassId
-    intancesIds[klassId] = -1
-    klassesMap[klassId] = klass
-    return klass
-  }
-  
-  function extendCallback(item, key){
-    this[key] = item
-  }
-
-  return classExports
-
-})
+}
--- test/class.js
+++ test/class.js
@@ -1,10 +1,9 @@
 var tape = require("tape")
   , klass = require("../")
-  , getKeys = Object.keys || function(o){var a = []; for(var i in o) if(o.hasOwnProperty(i)) a.push(i); return a}
 
 tape("class", function(test){
 
-  var properties = {
+  var myKlass = klass.extend({
         constructor : function(foo){
           if(arguments.length) {
             this.foo = foo
@@ -14,17 +13,14 @@ tape("class", function(test){
           this.foo = null
         },
         foo : "bar"
-      }
-    , myKlass = klass.extend(properties)
+      })
     , first = myKlass.create()
 
-  test.equal(myKlass.foo, properties.foo, "extended (prop)")
-  test.equal(myKlass.constructor, properties.constructor, "extended (ctor)")
-  test.equal(typeof parseInt(myKlass._klass, 10), "number", "has klass id")
-  test.equal(myKlass.create()._klass, myKlass._klass, "instance has klass id")
-  test.equal(typeof first._id, "number", "instance has unique instance id")
+  test.equal(myKlass.foo, "bar", "extended (prop)")
+  test.notEqual(myKlass.constructor.toString().indexOf("foo"), -1, "extended (ctor)")
   first.destroy()
   test.equal(first.foo, null, "destructor is called on destroy")
+  test.ok(first.extend().hasOwnProperty("constructor"), "adds constructor if not extended")
   test.end()
 
 })
